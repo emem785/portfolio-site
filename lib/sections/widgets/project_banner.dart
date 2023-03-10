fa39309f96d44341f4076358a1394c53527d1746
Emem <ememisong62@gmail.com>
2023-03-10 18:23:15 +0100

    Fix responsive mobile error

--- lib/sections/widgets/project_banner.dart
+++ lib/sections/widgets/project_banner.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import 'project_overlay.dart';
 
@@ -6,6 +7,7 @@ class ProjectBannerThree extends StatelessWidget {
   final String projectTitle;
   final String assetPath;
   final String projectKey;
+
   const ProjectBannerThree({
     Key? key,
     required this.assetPath,
@@ -16,6 +18,7 @@ class ProjectBannerThree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final showContainer = ValueNotifier(false);
+
     return MouseRegion(
       onEnter: (event) => showContainer.value = true,
       onExit: (event) => showContainer.value = false,
@@ -23,22 +26,28 @@ class ProjectBannerThree extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
         child: Stack(
+          alignment: Alignment.center,
           children: [
             SizedBox(
               height: 300,
               child: Image(image: AssetImage(assetPath)),
             ),
-            ValueListenableBuilder<bool>(
-              valueListenable: showContainer,
-              builder: (_, show, __) {
-                return show
-                    ? ProjectOverlay(
-                        projectTitle: projectTitle,
-                        projectKey: projectKey,
-                      )
-                    : const SizedBox();
-              },
-            )
+            ResponsiveWrapper.of(context).isSmallerThan(TABLET)
+                ? ProjectOverlay(
+                    projectTitle: projectTitle,
+                    projectKey: projectKey,
+                  )
+                : ValueListenableBuilder<bool>(
+                    valueListenable: showContainer,
+                    builder: (_, show, __) {
+                      return show
+                          ? ProjectOverlay(
+                              projectTitle: projectTitle,
+                              projectKey: projectKey,
+                            )
+                          : const SizedBox();
+                    },
+                  )
           ],
         ),
       ),
