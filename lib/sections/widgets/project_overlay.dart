83c1fafed06554ff1f6a1317e3e01eb5447720ca
Emem <ememisong62@gmail.com>
2023-03-30 13:04:14 +0100

    Fix image quality

--- lib/sections/widgets/project_overlay.dart
+++ lib/sections/widgets/project_overlay.dart
@@ -16,48 +16,51 @@ class ProjectOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      width: 400,
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.5),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            projectTitle,
-            style: Theme.of(context).textTheme.headline1,
-          ),
-          const Gap(8),
-          ElevatedButton(
-            onPressed: () {
-              context.pushRoute(ProjectWrapperRoute(title: projectKey));
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: ColorPalette.backgroundColor2,
-              elevation: 0.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4),
-              ),
+    return InkWell(
+      onTap: () => context.pushRoute(ProjectWrapperRoute(title: projectKey)),
+      child: Container(
+        height: ResponsiveWrapper.of(context).isSmallerThan(TABLET) ? 210 : 300,
+        width: 400,
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.5),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              projectTitle,
+              style: Theme.of(context).textTheme.headline1,
             ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Text("See More"),
-                  Gap(4),
-                  Icon(
-                    Icons.arrow_forward,
-                    size: 12.0,
-                  ),
-                ],
+            const Gap(8),
+            ElevatedButton(
+              onPressed: () =>
+                  context.pushRoute(ProjectWrapperRoute(title: projectKey)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: ColorPalette.backgroundColor2,
+                elevation: 0.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
               ),
-            ),
-          )
-        ],
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Text("See More"),
+                    Gap(4),
+                    Icon(
+                      Icons.arrow_forward,
+                      size: 12.0,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
